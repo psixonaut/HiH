7a1f98ac4301552ce958529c06ea57082b78cb4b
NikTak777 <nikkondrakhin@gmail.com>
2025-11-17 02:24:32 +0300

    Fixed tab flipping bug. No white flashes

--- app/src/main/java/com/example/rustoreapplicationshowcases/ui/theme/Theme.kt
+++ app/src/main/java/com/example/rustoreapplicationshowcases/ui/theme/Theme.kt
@@ -64,9 +64,14 @@ fun RuStoreApplicationShowcasesTheme(
 
     MaterialTheme(
         colorScheme = colorScheme,
-        typography = Typography,
-        content = content
-    )
+        typography = Typography
+    ) {
+        androidx.compose.material3.Surface(
+            color = colorScheme.background
+        ) {
+            content()
+        }
+    }
 }
 
 /**
